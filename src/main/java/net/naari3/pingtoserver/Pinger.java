b4ff5ad848cf0a92abc37bacd86e12a23d17db67
Yuuki Sone <yuki_sone@dwango.co.jp>
2020-01-13 11:12:23 +0900

    Add logger for pinger

--- src/main/java/net/naari3/pingtoserver/Pinger.java
+++ src/main/java/net/naari3/pingtoserver/Pinger.java
@@ -1,9 +1,14 @@
 package net.naari3.pingtoserver;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 import java.net.InetAddress;
 
 public class Pinger {
+    private static final Logger LOGGER = LogManager.getLogger();
+
     private String host;
     private int timeout = 5000;
 
@@ -29,8 +34,11 @@ public class Pinger {
 
         if (isReachable) {
             this.responseTime = end - start;
+            LOGGER.debug(String.format("%s response: %d ms", this.host, this.responseTime));
         } else {
             this.responseTime = -1;
+            LOGGER.warn(String.format("%s timeout"));
+
         }
         return isReachable;
     }
